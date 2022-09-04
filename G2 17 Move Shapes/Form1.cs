7ac22c2d2301c6a11d567eba294a687aca47fbae
chloehp <56298465+chloehp@users.noreply.github.com>
2022-09-04 17:27:49 +0100

    Add files via upload

---
+++ G2 17 Move Shapes/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace G2_17_Move_Shapes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Left = pictureBox1.Left + 5;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Left = pictureBox1.Left - 5;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top = pictureBox1.Top + 5;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top = pictureBox1.Top - 5;
+        }
+    }
+}
