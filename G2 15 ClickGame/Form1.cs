3d8f132e31daf760437ea95f89121b98b5ad5584
chloehp <56298465+chloehp@users.noreply.github.com>
2022-09-04 17:25:33 +0100

    Add files via upload

---
+++ G2 15 ClickGame/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace G2_15_ClickGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int theScore = Convert.ToInt16(label1.Text);
+
+            theScore = theScore + 1;
+
+            label1.Text = Convert.ToString(theScore);
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int theTimer = Convert.ToInt16(label2.Text);
+
+            theTimer = theTimer - 1;
+
+            label2.Text = Convert.ToString(theTimer);
+
+            if (theTimer == 0)
+            {
+                timer1.Enabled = false;
+                button1.Enabled = false;
+            }
+
+        }
+    }
+}
